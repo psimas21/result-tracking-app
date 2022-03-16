bf6cb525d585a21b4e7f42386363fdd373d13bd3
psimas21 <yescs4real@gmail.com>
2022-03-16 15:18:58 +0100

    update roles migration

--- database/migrations/2022_03_15_190412_create_roles_table.php
+++ database/migrations/2022_03_15_190412_create_roles_table.php
@@ -15,6 +15,8 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->string('role');
+            $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
     }
